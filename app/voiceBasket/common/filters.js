34182f09e9d10341365bc95bcbf9b82e5c75169f
rajatvijay <rajatvijay5@gmail.com>
2017-05-05 18:44:32 +0530

    language ordering

--- app/voiceBasket/common/filters.js
+++ app/voiceBasket/common/filters.js
@@ -21,3 +21,20 @@ app.filter('camelCaseToSpace', function () {
       .split('_').join(' ');
   };
 });
+
+app.filter('removeDoubleUnderscores', function () {
+  return function (str) {
+    return str
+      .split('_')
+      .slice(1)
+      .join('_');
+  };
+});
+
+app.filter('toArray', function() { return function(obj) {
+  if (!(obj instanceof Object)) return obj;
+  return _.map(obj, function(val, key) {
+    return Object.defineProperty(val, '$key', {__proto__: null, value: key});
+  });
+}});
+
